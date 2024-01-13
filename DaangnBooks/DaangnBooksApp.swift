13608120739f3d402ecbeb4c8eebcb90ccc7f8b5
타피_이상호 <shotclock@afreecatv.com>
2024-01-13 12:10:34 +0900

    Initial Commit

---
+++ DaangnBooks/DaangnBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaangnBooksApp.swift
+//  DaangnBooks
+//
+//  Created by lee sangho on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct DaangnBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
